5bf91e3cca75c5fc08943f758a1f49a6d53fe7d6
patrik-fm <skateandflowers@gmail.com>
2025-06-19 18:55:49 +0200

    added assets structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 export default function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addPassthroughCopy("src/css/")
+    eleventyConfig.addWatchTarget("src/css/")
     return {
         dir: {
             input: 'src',
